ec956dd504f050c03e3b853f48272f592c1f78d5
mmontgomery849 <mjmontgomery@fullsail.edu>
2014-11-07 10:28:14 -0500

    commit

--- ContitionalsAssignment/Conditionals_Personal/scripts/script.js
+++ ContitionalsAssignment/Conditionals_Personal/scripts/script.js
@@ -12,15 +12,35 @@ function validateNumber(){
         validateNumber();
         }else if(Number(entry) >= 1 && Number(entry) <= 100){
         alert("Thank you!")
-    }
 
+    }
     else{
         alert("Sorry. We were unable to process your entry. Let's try this again");
         validateNumber();
     }
-
+    var entry2 = prompt("Now, please enter a number to multiply " + entry + " by.");
+    if(entry2.length == 0){
+        alert("You did not enter anything. Let's start from the beginning.");
+        validateNumber();
+    }else if(Number(entry2) < 1 || Number(entry2) > 100){
+        alert("You did not enter a number between between 1 and 100. Let's start from the beginning.");
+        validateNumber();
+    }
+    var product = Number(entry) * Number(entry2);
+    alert(entry + " multiplied by " + entry2 + " equals " + product + ".");
+    alert("Let's do some other stuff with the first number you chose (" + entry +").");
+    if(entry.length == 1){
+        var plural = "digit"
+    }else if(entry.length == 2){
+        var plural = "digits"
+    }
+    alert("We know that " + entry + " consists of " + entry.length + " " + plural + ".")
 }
 validateNumber();
+
+
+
+
 
 
 
