a8bf1b5d4ce122bdb065c85ad2426f03ebf0a3bc
mmontgomery849 <mjmontgomery@fullsail.edu>
2014-11-09 00:21:45 -0500

    commit

--- ContitionalsAssignment/Conditionals_Industry/scripts/script.js
+++ ContitionalsAssignment/Conditionals_Industry/scripts/script.js
@@ -1,16 +1,17 @@
 /**
  * Created by michael on 11/09/14.
  */
-function task() {
+
     alert("Thank you for visiting my page!");//greeting
     alert("In the technology industry, screen and digital image quality are increasingly important factors");
     alert("You can use this tool to calculate the PPI or pixel density of a digital display");
     //obtain screen dimensions from user input and assign variables to given values
+function task() {
     var height = prompt("Start by entering the number of pixels that your screen is high:");
     var length = prompt("Got it; thanks! Now, enter the number of pixels that your screen is wide:");
     //validate input and ensure that input meets required criteria
     if (height > length){
-        alert("You stated that your display if taller than it is wide. This is highly unlikely. Let's start from the beginning.");
+        alert("You stated that your display is taller than it is wide. This is highly unlikely. Let's start from the beginning.");
         task();
     }else if(height.length == 0 || length.length == 0){
         alert("One of your entries was blank. Let's start from the beginning.");
