8335466566656005048d3cb6b7960e98c1376a95
mmontgomery849 <mjmontgomery@fullsail.edu>
2014-11-09 00:07:42 -0500

    commit

--- ContitionalsAssignment/Conditionals_Industry/scripts/script.js
+++ ContitionalsAssignment/Conditionals_Industry/scripts/script.js
@@ -1,3 +1,12 @@
 /**
- * Created by michael on 10/21/14.
+ * Created by michael on 11/09/14.
  */
+alert("Thank you for visiting my page!");
+alert("In the technology industry, screen and digital image quality are increasingly important factors");
+alert("You can use this tool to calculate the PPI or pixel density of a digital display");
+var height=prompt("Start by entering the number of pixels that your screen is high:");
+var length=prompt("Got it; thanks! Now, enter the number of pixels that your screen is wide:");
+var inches=prompt("Thanks, again! Please enter the number of inches that your screen is:");
+var ppi=(Number(height) * Number(length)) / Number(inches);
+console.log("Since your display is " + height + " pixels high and " + length + " pixels wide, the pixel density of your display is " + ppi + ".");
+document.write("Since your display is " + height + " pixels high and " + length + " pixels wide, the pixel density of your display is " + ppi + ".");
