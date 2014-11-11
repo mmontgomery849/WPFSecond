b4bd7a2ffa41232a5b7b62e4fe34568f6de030b4
mmontgomery849 <mjmontgomery@fullsail.edu>
2014-11-11 07:56:22 -0500

    commit

--- Functions_Worksheet/scripts/script.js
+++ Functions_Worksheet/scripts/script.js
@@ -1,12 +1,17 @@
 /**
  * Created by michael on 11/11/14.
  */
-var x = function entry(){
+function entry() {
     var rad = prompt("please enter the radius of the circle to find the circumference.");
-    if(rad.length == 0){
+    if (rad.length == 0) {
         alert("You did not enter anything. Let's try this again.");
         entry();
     }
     var circ = (Number(rad) * 2) * Math.PI;
-    return circ
+    return circ;
+
+
+    console.log("The circumference of the circle is " + circ + ".");
+    document.write("The circumference of the circle is " + circ + ".");
 }
+entry();
