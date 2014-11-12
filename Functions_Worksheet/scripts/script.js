0728c4788dab5d0998d5f28f8833fdbe54164ccd
mmontgomery849 <mjmontgomery@fullsail.edu>
2014-11-12 10:36:17 -0500

    commit

--- Functions_Worksheet/scripts/script.js
+++ Functions_Worksheet/scripts/script.js
@@ -14,5 +14,17 @@ function entry() {
     alert("The circumference of the circle is " + Math.round(circ) + ".");
     console.log("The circumference of the circle is " + Math.round(circ) + ".");
     document.write("The circumference of the circle is " + Math.round(circ) + ".");
+    repeat();
+
+    function repeat() {
+        var repeater = prompt("If you would like to repeat this activity, type 'again'. To proceed to the next activity, type 'next'.");
+        if (repeater == "again") {
+            entry();
+        } else if (repeater == "next") {
+            stings();
+        } else if (repeater != "again" || repeater != "next") {
+            repeat();
+        }
+    }
 }
 entry();
