76fef79e523c4ecd804f133f9b0d9ea43f57c3fc
mmontgomery849 <mjmontgomery@fullsail.edu>
2014-11-12 10:08:38 -0500

    commit

--- Functions_Worksheet/scripts/script.js
+++ Functions_Worksheet/scripts/script.js
@@ -3,12 +3,12 @@
  */
 function entry() {
     var rad = prompt("please enter the radius of the circle to find the circumference.");
-    if (rad.length == 0) {
-        alert("You did not enter anything. Let's try this again.");
-        entry();
-    }else if(isNaN(rad)){
-        alert("Please enter a number.");
-        entry();
+    while (rad.length == 0) {
+        var rad = prompt("You did not enter anything. Please enter a number.");
+
+    }while(isNaN(rad)){
+        var rad = prompt("Your entry should be all numeric. Please enter a number.");
+
     }
     var circ = (Number(rad) * 2) * Math.PI;
     alert("The circumference of the circle is " + circ + ".");
